86e4a3509ae5bae828fb8505d870bf1b812421ba
daisy0905 <dtong0905@gmail.com>
2020-11-21 22:07:52 -0700

    Created api requests for users and login

--- app.py
+++ app.py
@@ -32,9 +32,14 @@ def users():
                 headers = [i[0] for i in cursor.description]
                 for row in rows:
                     users.append(dict(zip(headers, row)))
-        except Exception as error:
-            print("Something went wrong (THIS IS LAZAY): ")
-            print(error)
+        except mariadb.dataError:
+            print("There seems to be something wrong with your data.")
+        except mariadb.databaseError:
+            print("There seems to be something wrong with your database.")
+        except mariadb.ProgrammingError:
+            print("There seems to be something wrong with SQL written.")
+        except mariadb.OperationalError:
+            print("There seems to be something wrong with the connection.")
         finally:
             if(cursor != None):
                 cursor.close()
@@ -54,29 +59,39 @@ def users():
         user_birthdate = request.json.get("birthdate")
         user_bio = request.json.get("bio")
         user_email = request.json.get("email")
-        user_url = request.json.get("url")
+        user_image = request.json.get("image")
         rows = None
         user = None
         try:
             conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, port=dbcreds.port, database=dbcreds.database, host=dbcreds.host)
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO users(username, password, birthdate, bio, email, url) VALUES(?, ?, ?, ?, ?, ?)", [user_username, user_password, user_birthdate, user_bio, user_email, user_url])
+            cursor.execute("INSERT INTO users(username, password, birthdate, bio, email, image) VALUES(?, ?, ?, ?, ?, ?)", [user_username, user_password, user_birthdate, user_bio, user_email, user_image])
             conn.commit()
             rows = cursor.rowcount
             print(rows)
-            cursor.execute("SELECT * FROM users WHERE username=? AND password=?", [user_username, user_password])
+            cursor.execute("SELECT * FROM users WHERE username=?", [user_username])
             user_row = cursor.fetchone()
-            token = random.randint(0, 10000000)
+            user_id = user_row[0]
+            print(user_id)
             user = {}
             headers = [i[0] for i in cursor.description]
             user = dict(zip(headers, user_row))
-            user["loginToken"] = token
-            print(token)
-            print(user)
-            # cursor.execute("INSERT INTO user_session ")
-        except Exception as error:
-            print("Something went wrong (THIS IS LAZAY): ")
-            print(error)
+            token = random.randint(0, 10000000)
+            cursor.execute("INSERT INTO user_session(loginToken, user_id) VALUES(?, ?)", [token, user_id])
+            conn.commit()
+            rows = cursor.rowcount
+            if rows == 1:
+                user["loginToken"] = token
+                print(token)
+                print(user)
+        except mariadb.dataError:
+            print("There seems to be something wrong with your data.")
+        except mariadb.databaseError:
+            print("There seems to be something wrong with your database.")
+        except mariadb.ProgrammingError:
+            print("There seems to be something wrong with SQL written.")
+        except mariadb.OperationalError:
+            print("There seems to be something wrong with the connection.")
         finally:
             if(cursor != None):
                 cursor.close()
@@ -91,50 +106,101 @@ def users():
     elif request.method == 'PATCH':
         conn = None
         cursor = None
-        loginToken = request.json.get("token")
+        token = request.json.get("token")
         user_username = request.json.get("username")
         user_password = request.json.get("password")
         user_birthdate = request.json.get("birthdate")
         user_bio = request.json.get("bio")
         user_email = request.json.get("email")
-        user_url = request.json.get("url")
+        user_image = request.json.get("image")
         rows = None
+        user = None
+        try:
+            conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, port=dbcreds.port, database=dbcreds.database, host=dbcreds.host)
+            cursor = conn.cursor() 
+            cursor.execute("SELECT * FROM user_session WHERE loginToken=?", [token,])
+            user = cursor.fetchone()
+            print(user)
+            if user != None and user != []:
+                user_id = user[2]
+                print(user_id)
+                if user_username != "" and user_username != None:
+                    cursor.execute("UPDATE users SET username=? WHERE id=?", [user_username, user_id])
+                if user_password != "" and user_password != None:
+                    cursor.execute("UPDATE users SET password=? WHERE id=?", [user_password, user_id])
+                if user_birthdate != "" and user_birthdate != None:
+                    cursor.execute("UPDATE users SET birthdate=? WHERE id=?", [user_birthdate, user_id])
+                if user_bio != "" and user_bio != None:
+                    cursor.execute("UPDATE users SET bio=? WHERE id=?", [user_bio, user_id])
+                if user_email != "" and user_email != None:
+                    cursor.execute("UPDATE users SET email=? WHERE id=?", [user_email, user_id])
+                if user_image != "" and user_image != None:
+                    cursor.execute("UPDATE users SET image=? WHERE id=?", [user_image, user_id])
+                conn.commit()
+                rows = cursor.rowcount
+                cursor.execute("SELECT * FROM users WHERE id=?", [user_id])
+                user_row = cursor.fetchone()
+                users = {}
+                headers = [i[0] for i in cursor.description]
+                users = dict(zip(headers, user_row))
+        except mariadb.dataError:
+            print("There seems to be something wrong with your data.")
+        except mariadb.databaseError:
+            print("There seems to be something wrong with your database.")
+        except mariadb.ProgrammingError:
+            print("There seems to be something wrong with SQL written.")
+        except mariadb.OperationalError:
+            print("There seems to be something wrong with the connection.")
+        finally:
+            if(cursor != None):
+                cursor.close()
+            if(conn != None):
+                conn.rollback()
+                conn.close()
+            if(rows == 1):
+                return Response(json.dumps(users, default=str), mimetype="application/json", status=201)
+            else:
+                return Response("Something went wrong!", mimetype="text/html", status=500)
 
+    # elif request.method == 'DELETE'
 
-    
-
-@app.route('/api/login', methods=['POST', 'DELETE'])
+@app.route('/login', methods=['POST', 'DELETE'])
 def login():
     if request.method == 'POST':
         conn = None
         cursor = None
         user_username = request.json.get("username")
         user_password = request.json.get("password")
-        users = None
         rows = None
         user = None
         try:
             conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, port=dbcreds.port, database=dbcreds.database, host=dbcreds.host)
             cursor = conn.cursor()
             cursor.execute("SELECT * FROM users WHERE username=? AND password=?", [user_username, user_password])
-            users = cursor.fetchall()
-            print(users)
-            if users != None and users != []:
-                user_id = users[0][0]
+            user_row = cursor.fetchone()
+            print(user_row)
+            user = {}
+            headers = [i[0] for i in cursor.description]
+            user = dict(zip(headers, user_row))
+            if user_row != None and user_row != []:
+                user_id = user_row[0]
                 print(user_id)
                 token = random.randint(0, 10000000)
-                cursor.execute("INSERT INTO user_login(token, user_id) VALUES(?, ?)", [token, user_id])
+                cursor.execute("INSERT INTO user_session(loginToken, user_id) VALUES(?, ?)", [token, user_id])
                 conn.commit()
                 rows = cursor.rowcount
                 print(rows)
-                user = {
-                    "id": users[0][0],
-                    "username": users[0][1],
-                    "token": token
-                }
-        except Exception as error:
-            print("Something went wrong (THIS IS LAZAY): ")
-            print(error)
+                user["loginToken"] = token
+                print(token)
+                print(user)      
+        except mariadb.dataError:
+            print("There seems to be something wrong with your data.")
+        except mariadb.databaseError:
+            print("There seems to be something wrong with your database.")
+        except mariadb.ProgrammingError:
+            print("There seems to be something wrong with SQL written.")
+        except mariadb.OperationalError:
+            print("There seems to be something wrong with the connection.")
         finally:
             if(cursor != None):
                 cursor.close()
@@ -157,9 +223,14 @@ def login():
             cursor.execute("DELETE FROM user_login WHERE token=?", [post_token,])
             conn.commit()
             rows = cursor.rowcount
-        except Exception as error:
-            print("Something went wrong (THIS IS LAZAY): ")
-            print(error)
+        except mariadb.dataError:
+            print("There seems to be something wrong with your data.")
+        except mariadb.databaseError:
+            print("There seems to be something wrong with your database.")
+        except mariadb.ProgrammingError:
+            print("There seems to be something wrong with SQL written.")
+        except mariadb.OperationalError:
+            print("There seems to be something wrong with the connection.")
         finally:
             if cursor != None:
                 cursor.close()
